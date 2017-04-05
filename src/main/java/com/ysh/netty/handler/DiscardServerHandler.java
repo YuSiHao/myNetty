4321ceee3000857a5e9a0517712d1741414c1dc4
chandler <1941719093@qq.com>
2017-04-05 13:14:36 +0800

    set up successful

---
+++ src/main/java/com/ysh/netty/handler/DiscardServerHandler.java
@@ -0,0 +1,26 @@
+package com.ysh.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+	/**
+	 * 处理服务端 channel.
+	 */
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+		// 默默地丢弃收到的数据
+		System.out.println("go in");
+		((ByteBuf) msg).release(); // (3)
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+		// 当出现异常就关闭连接
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
